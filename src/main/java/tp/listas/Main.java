54567b1e267fee93eba8d73a8f221403731da9b0
Bernard Maximiliano <bmaximiliano0210@gmail.com>
2025-02-16 20:05:20 -0300

    refactor: structure tests

--- src/main/java/tp/listas/Main.java
+++ src/main/java/tp/listas/Main.java
@@ -2,10 +2,37 @@ package tp.listas;
 
 import tp.listas.scenarios.Scenario;
 
+import java.util.List;
+
 public class Main {
     static int operationsPerThread = 15;
     static int threads = 15000;
     static int probabilityAdd = 50;
+
+    public enum ListType {
+        FINE_GRAINED(0),
+        OPTIMISTIC(1),
+        NON_BLOCKING(2);
+
+        private final int value;
+
+        ListType(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static ListType fromValue(int value) {
+            for (ListType type : ListType.values()) {
+                if (type.getValue() == value) {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Invalid value: " + value);
+        }
+    }
 
     public static void main(String[] args) {
 
@@ -34,69 +61,49 @@ public class Main {
     }
 
     public static void Test() {
+
+        // Una dimension para variar la lista, en el arreglo dado por mkln (0, 1, 2)
+        // 0 -> FineGrainedList
+        // 1 -> OptimisticList
+        // 2 -> NonBlockingList
+        // Una dimension para variar la probabilidad de agregar (m)
+        // Una dimension para variar la cantidad de threads (k)
+        // Una dimension para variar la cantidad de operaciones por thread (l)
+        // Una dimension para variar la cantidad de tests (n)
+
+        int probabilities[] = {0, 50, 100};
+        int threads[] = {7500, 15000, 30000};
+        int operationsPerThread[] = {5, 10, 20};
+        float times[][][][][] = new float[3][3][3][3][3];
         System.out.println("Trash test...");
-        NoAddingScenarios();
-        OnlyAddingScenarios();
-        BalancedScenarios();
-        float times[][][] = new float[3][3][3];
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Testing...");
-            times[i][0] = NoAddingScenarios();
-            times[i][1] = OnlyAddingScenarios();
-            times[i][2] = BalancedScenarios();
+        RunScenarios(threads[0], operationsPerThread[0], probabilities[0]);
+        System.out.println("Starting tests....");
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                for (int m = 0; m < 3; m++) {
+                    var currentThread = threads[k];
+                    var currentOperationsPerThread = operationsPerThread[l];
+                    var currentProbabilityAdd = probabilities[m];
+                    for (int n = 0; n < 3; n++) {
+                        times[k][l][m][n] = RunScenarios(currentThread, currentOperationsPerThread, currentProbabilityAdd);
+                        ;
+                    }
+                    System.out.println("Finished tests with threads: " + currentThread + ", operations per thread: " + currentOperationsPerThread + ", probability add: " + currentProbabilityAdd);
+                }
+            }
+        }
+        System.out.println("Printing results...");
+        for (int k = 0; k < 3; k++) {
+            for (int l = 0; l < 3; l++) {
+                for (int m = 0; m < 3; m++) {
+                    for (int i = 0; i < 3; i++) {
+                        System.out.println("Results for test " + ListType.fromValue(i) + " with threads: " + threads[k] + ", operations per thread: " + operationsPerThread[l] + ", probability add: " + probabilities[m]);
+                        printTestResults(times[k][l][m][0][i], times[k][l][m][1][i], times[k][l][m][2][i]);
+                    }
+                }
+            }
         }
 
-        float firstTime = times[0][0][0], secondTime = times[1][0][0], thirdTime = times[2][0][0];
-        System.out.println("Scenario: fine grained list, no adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][1][0];
-        secondTime = times[1][1][0];
-        thirdTime = times[2][1][0];
-        System.out.println("Scenario: fine grained list, only adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][2][0];
-        secondTime = times[1][2][0];
-        thirdTime = times[2][2][0];
-        System.out.println("Scenario: fine grained list, balanced, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][0][1];
-        secondTime = times[1][0][1];
-        thirdTime = times[2][0][1];
-        System.out.println("Scenario: optimistic list, no adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][1][1];
-        secondTime = times[1][1][1];
-        thirdTime = times[2][1][1];
-        System.out.println("Scenario: optimistic list, only adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][2][1];
-        secondTime = times[1][2][1];
-        thirdTime = times[2][2][1];
-        System.out.println("Scenario: optimistic list, balanced, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][0][2];
-        secondTime = times[1][0][2];
-        thirdTime = times[2][0][2];
-        System.out.println("Scenario: non blocking list, no adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][1][2];
-        secondTime = times[1][1][2];
-        thirdTime = times[2][1][2];
-        System.out.println("Scenario: non blocking list, only adding, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
-
-        firstTime = times[0][2][2];
-        secondTime = times[1][2][2];
-        thirdTime = times[2][2][2];
-        System.out.println("Scenario: non blocking list, balanced, " + threads + ", 5 ops/thread");
-        printTestResults(firstTime, secondTime, thirdTime);
 
     }
 
@@ -107,223 +114,19 @@ public class Main {
         System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
     }
 
-    public static float[] NoAddingScenarios() {
+    public static float[] RunScenarios(int thCount, int opsPerThread, int probAdd) {
         float times[] = new float[3];
         FineGrainedList<Integer> fineGrainedList = new FineGrainedList<>();
-        Scenario noAdding = new Scenario(threads, operationsPerThread, 0, fineGrainedList);
+        Scenario noAdding = new Scenario(thCount, opsPerThread, probAdd, fineGrainedList);
         times[0] = noAdding.run();
         OptimisticList<Integer> optimisticList = new OptimisticList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, optimisticList);
+        noAdding = new Scenario(thCount, opsPerThread, probAdd, optimisticList);
         times[1] = noAdding.run();
         NonBlockingList<Integer> nonBlockingList = new NonBlockingList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, nonBlockingList);
+        noAdding = new Scenario(thCount, opsPerThread, probAdd, nonBlockingList);
         times[2] = noAdding.run();
         return times;
     }
 
-    //
-    public static float[] OnlyAddingScenarios() {
-        float times[] = new float[3];
-        FineGrainedList<Integer> fineGrainedList = new FineGrainedList<>();
-        Scenario onlyAdding = new Scenario(threads, operationsPerThread, 100, fineGrainedList);
-        times[0] = onlyAdding.run();
-        OptimisticList<Integer> optimisticList = new OptimisticList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, optimisticList);
-        times[1] = onlyAdding.run();
-        NonBlockingList<Integer> nonBlockingList = new NonBlockingList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, nonBlockingList);
-        times[2] = onlyAdding.run();
-        return times;
-    }
 
-    public static float[] BalancedScenarios() {
-        float times[] = new float[3];
-        FineGrainedList<Integer> fineGrainedList = new FineGrainedList<>();
-        Scenario balanced = new Scenario(threads, operationsPerThread, probabilityAdd, fineGrainedList);
-        times[0] = balanced.run();
-        OptimisticList<Integer> optimisticList = new OptimisticList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, optimisticList);
-        times[1] = balanced.run();
-        NonBlockingList<Integer> nonBlockingList = new NonBlockingList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, nonBlockingList);
-        times[2] = balanced.run();
-        return times;
-    }
-
-    public static void FineGrainedListScenarios() {
-
-        System.out.println("Scenario: fine grained list, no adding, " + threads + ", 5 ops/thread");
-        FineGrainedList<Integer> fineGrainedList = new FineGrainedList<>();
-        Scenario noAdding = new Scenario(threads, operationsPerThread, 0, fineGrainedList);
-        float firstTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, fineGrainedList);
-        float secondTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, fineGrainedList);
-        float thirdTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: fine grained list, only adding, " + threads + ", 5 ops/thread");
-        fineGrainedList = new FineGrainedList<>();
-        Scenario onlyAdding = new Scenario(threads, operationsPerThread, 100, fineGrainedList);
-        firstTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, fineGrainedList);
-        secondTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, fineGrainedList);
-        thirdTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: fine grained list, balanced, " + threads + ", 5 ops/thread");
-        fineGrainedList = new FineGrainedList<>();
-        Scenario balanced = new Scenario(threads, operationsPerThread, probabilityAdd, fineGrainedList);
-        firstTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, fineGrainedList);
-        secondTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        fineGrainedList = new FineGrainedList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, fineGrainedList);
-        thirdTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-    }
-
-    public static void OptimisticListScenarios() {
-
-        System.out.println("Scenario: optimistic list, no adding, " + threads + ", 5 ops/thread");
-        OptimisticList<Integer> optimisticList = new OptimisticList<>();
-        Scenario noAdding = new Scenario(threads, operationsPerThread, 0, optimisticList);
-        float firstTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, optimisticList);
-        float secondTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, optimisticList);
-        float thirdTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: optimistic list, only adding, " + threads + ", 5 ops/thread");
-        optimisticList = new OptimisticList<>();
-        Scenario onlyAdding = new Scenario(threads, operationsPerThread, 100, optimisticList);
-        firstTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, optimisticList);
-        secondTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, optimisticList);
-        thirdTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: optimistic list, balanced, " + threads + ", 5 ops/thread");
-        optimisticList = new OptimisticList<>();
-        Scenario balanced = new Scenario(threads, operationsPerThread, probabilityAdd, optimisticList);
-        firstTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, optimisticList);
-        secondTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        optimisticList = new OptimisticList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, optimisticList);
-        thirdTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-    }
-
-    public static void NonBlockingListScenarios() {
-
-        System.out.println("Scenario: non blocking list, no adding, " + threads + ", 5 ops/thread");
-        NonBlockingList<Integer> nonBlockingList = new NonBlockingList<>();
-        Scenario noAdding = new Scenario(threads, operationsPerThread, 0, nonBlockingList);
-        float firstTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, nonBlockingList);
-        float secondTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        noAdding = new Scenario(threads, operationsPerThread, 0, nonBlockingList);
-        float thirdTime = noAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: non blocking list, only adding, " + threads + ", 5 ops/thread");
-        nonBlockingList = new NonBlockingList<>();
-        Scenario onlyAdding = new Scenario(threads, operationsPerThread, 100, nonBlockingList);
-        firstTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, nonBlockingList);
-        secondTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        onlyAdding = new Scenario(threads, operationsPerThread, 100, nonBlockingList);
-        thirdTime = onlyAdding.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-
-
-        System.out.println("Scenario: non blocking list, balanced, " + threads + ", 5 ops/thread");
-        nonBlockingList = new NonBlockingList<>();
-        Scenario balanced = new Scenario(threads, operationsPerThread, probabilityAdd, nonBlockingList);
-        firstTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "First test:", firstTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, nonBlockingList);
-        secondTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Second test:", secondTime, " seconds");
-
-        nonBlockingList = new NonBlockingList<>();
-        balanced = new Scenario(threads, operationsPerThread, probabilityAdd, nonBlockingList);
-        thirdTime = balanced.run();
-        System.out.printf("%15s%10.4f%s\n", "Third test:", thirdTime, " seconds");
-
-        System.out.printf("%15s%10.4f%s\n", "Avg:", (firstTime + secondTime + thirdTime) / 3, " seconds");
-    }
 }
